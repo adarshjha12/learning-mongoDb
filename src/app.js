f317c92fc0022d1ea359b84b67d317bfa6cd4749
adarsh <jhaa50872@gmail.com>
2024-07-17 20:36:22 +0530

    schema defined

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 let mongoose = require('mongoose')
 
-// console.log(typeof mongoose);
-mongoose.connect('mongodb://localhost:27017/newdb').then( () => console.log('connection successful'))
+// creating databsase locally
+mongoose.connect('mongodb://localhost:27017/adarshDb')
+.then( () => console.log('connection successful'))
+.catch((err) => console.log(err))
+
+// defining structure of the document
+mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    class: String,
+    active: Boolean,
+    roll_no: Number,
+    date:{
+        type: Date,
+        default: Date.now
+    }
+
+
+})
