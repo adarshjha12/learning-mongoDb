7cf66afea900fb169e877a3a8168ffd98570c38e
adarsh <jhaa50872@gmail.com>
2024-07-17 19:21:05 +0530

    connected

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-cccccccttttttttttttttttt
+let mongoose = require('mongoose')
+
+// console.log(typeof mongoose);
+mongoose.connect('mongodb://localhost:27017/newdb').then( () => console.log('connection successful'))
